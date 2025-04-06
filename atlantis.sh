890eede545bd3b316751734fda690a1afab7c3a6
Javier Taibo <javier.taibo@gmail.com>
2025-04-06 17:00:16 +0000

    Added comments

--- atlantis.sh
+++ atlantis.sh
@@ -5,6 +5,8 @@ if [ `id -u` != 0 ]; then
   SUDO=sudo
 fi
 
+# Disable power saving for the WiFi interface, so it is always available to connect
+# atlantis host through the network
 $SUDO iw dev wlan0 set power_save off
 
 cd `dirname $0`
